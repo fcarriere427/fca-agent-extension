2af2f26fc43ed17e96bf905f9bbbe569ae1832f7
Florian <florian.carriere@gmail.com>
2025-04-27 00:18:46 +0200

    test

--- background/api-key.js
+++ background/api-key.js
@@ -2,7 +2,7 @@
 
 // Clé API fixe pour l'authentification avec le serveur
 // Cette clé doit correspondre exactement à celle configurée dans le fichier .env du serveur
-export const API_KEY = 'punaisecestrelou1998!';
+export const API_KEY = 'encoreuntest';
 
 // Fonction utilitaire pour récupérer la clé API
 export function getApiKey() {
